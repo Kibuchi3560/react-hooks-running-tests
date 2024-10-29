d192e6766f96126e586684bb941cab8bf9b8a104
Kibuchi3560 <jontezbuchiki3560@gmail.com>
2024-10-29 15:38:31 +0300

    Second commit

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -6,7 +6,7 @@ import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
-  screen.debug(element);
+ 
 
-  expect(element).toBeInTheDocument();
+  expect(screen.queryByText ("please pass this test")).toBeInTheDocument();
 });
